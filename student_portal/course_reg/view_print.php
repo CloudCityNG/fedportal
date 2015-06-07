17f6155c60931253de86c938f0db04420eac80a7
maneptha <maneptha@gmail.com>
2015-06-07 12:24:20 +0100

    student dashboard - course reg - don't display form printing

--- student_portal/course_reg/view_print.php
+++ student_portal/course_reg/view_print.php
@@ -1,59 +1,49 @@
-<div class="already-registered">
-  <h4>
-    You are signed up for courses for <?php echo $semester_text ?> semester
-    Please print course form if you have not done so.
-  </h4>
-  <span class='printer-friendly'>Click here for printer friendly view.</span>
-</div>
+<?php
+$sessionSemesterInfo = "{$semester_text} semester {$academicYear} session";
+?>
 
-<div class="view-print-courses">
+<div class="view-registered-courses">
 
-  <h4 class="text-center college-name"><?php echo strtoupper(SCHOOL_NAME) ?></h4>
-
-  <h4 class="text-center">
-    <strong>COURSE REGISTRATION FORM</strong> - DEPARTMENT OF <?php echo $dept_name; ?>
+  <h4 class="view-registered-courses-info">
+    You are signed up for courses for <?php echo $sessionSemesterInfo ?> Please
+    <a href="<?php echo STATIC_ROOT . 'student_portal/view_info/' ?>">print course form</a>
+    if you have not done so.
   </h4>
 
-  <table class="img-and-name">
-    <tbody>
-      <tr>
-        <td>
-          <img src="<?php echo get_photo($regNo, true); ?>" alt="<?php echo $student['names']; ?>"/>
-        </td>
+  <div class="view-registered-courses-course-data">
+    <h4 class="text-center">
+      <strong>COURSE REGISTRATION</strong>
+    </h4>
 
-        <td class="names">
-          <div>
-            <p>
-              <strong>
-                NAME OF STUDENT:&nbsp;&nbsp;
-              </strong>
-              <?php echo strtoupper($student['names']); ?>
-            </p>
+    <table class="table table-bordered table-responsive table-striped img-and-name">
+      <tbody>
+        <tr>
+          <th>NAME OF STUDENT</th>
+          <td><?php echo $student['names']; ?></td>
+        </tr>
 
-            <p>
-              <strong>REGISTRATION NO:&nbsp;&nbsp;</strong> <?php echo $regNo; ?>
-            </p>
+        <tr>
+          <th>REGISTRATION NO</th>
+          <td><?php echo $regNo; ?></td>
+        </tr>
 
-            <p>
-              <strong>
-                LEVEL:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              </strong>
-              <?php echo $currentLevel; ?>
-            </p>
+        <tr>
+          <th>DEPARTMENT</th>
+          <td><?php echo $dept_name; ?></td>
+        </tr>
 
-            <p>
-              <strong>
-                SESSION:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-              </strong>
-              <?php echo "{$semester_text} semester {$academicYear} session"; ?>
-            </p>
-          </div>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+        <tr>
+          <th>LEVEL</th>
+          <td><?php echo $currentLevel; ?></td>
+        </tr>
 
-  <div class="row">
+        <tr>
+          <th>SESSION</th>
+          <td><?php echo $sessionSemesterInfo ?></td>
+        </tr>
+      </tbody>
+    </table>
+
     <table class="table table-striped table-condensed table-bordered view-print-course-table">
 
       <thead>
@@ -62,18 +52,20 @@
           <th>Course Code</th>
           <th>Course Title</th>
           <th>Credit Unit</th>
-          <th>Lecturer Sign</th>
         </tr>
       </thead>
 
       <tbody>
         <?php
         $course_seq = 1;
+        $sumUnits = 0;
 
         foreach ($course_data as $courses) {
           $code = $courses['code'];
           $title = $courses['title'];
-          $unit = sprintf('%.2f', $courses['unit']);
+          $unit = floatval($courses['unit']);
+          $sumUnits += $unit;
+          $unit = sprintf('%.2f', $unit);
 
           echo "<tr>\n" .
 
@@ -85,33 +77,21 @@
 
             "<td class='text-center'>$unit</td>\n" .
 
-            '<td></td>' .
-
             "</tr>";
 
           $course_seq++;
         }
+
+        $sumUnits = number_format($sumUnits, 2);
+        echo "
+          <tr>
+            <td></td>
+            <td></td>
+            <td style='font-weight: bolder'>TOTAL</td>
+            <td class='text-center'>{$sumUnits}</td>
+          </tr>";
         ?>
-      </tbody>
-    </table>
-
-    <table class="table table-bordered signature-table">
-      <tbody>
-        <tr>
-          <th>HOD'S CONFIRMATION</th>
-
-          <td></td>
-
-          <th>ACADEMIC ADVISER</th>
-
-          <td></td>
-
-          <th>DEPUTY RECTOR</th>
-
-          <td></td>
-        </tr>
       </tbody>
     </table>
   </div>
 </div>
-<p class="back-to-main" style="display: none;">Back to main</p>
