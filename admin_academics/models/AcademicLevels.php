358931f43cc8a43908d3d4c7d687e3b01aefaa16
maneptha <maneptha@gmail.com>
2015-06-17 06:37:11 +0100

    AcademicLevels model uses sqllogger class for logging

--- admin_academics/models/AcademicLevels.php
+++ admin_academics/models/AcademicLevels.php
@@ -2,31 +2,42 @@
 
 require_once(__DIR__ . '/../../helpers/databases.php');
 require_once(__DIR__ . '/../../helpers/app_settings.php');
+require_once(__DIR__ . '/../../helpers/SqlLogger.php');
 
 
 class AcademicLevels
 {
   private static $LOG_NAME = 'AcademicLevelsModel';
 
-  public static function get_all_levels()
+  public static function getAllLevels()
   {
-    $log = get_logger(self::$LOG_NAME);
-
     $query = "SELECT * FROM academic_levels";
 
-    $log->addInfo("About to get all academic levels by executing the query : {$query}");
+    $logMsg = SqlLogger::makeLogMessage('get all academic levels', $query);
 
     $stmt = get_db()->query($query);
 
     if ($stmt) {
-      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+      SqlLogger::logStatementSuccess(self::logger(), $logMsg);
 
-      $log->addInfo("Statement executed successfully, result is: ", $result);
-      return $result;
+      $result = $stmt->fetchAll();
+
+      if (count($result)) {
+        SqlLogger::logDataRetrieved(self::logger(), $logMsg, $result);
+        return $result;
+      }
     }
 
-    $log->addWarning("Statement did not execute successfully");
+    SqlLogger::logNoData(self::logger(), $logMsg);
     return null;
+  }
+
+  /**
+   * @return \Monolog\Logger
+   */
+  private static function logger()
+  {
+    return get_logger('AcademicLevelsModel');
   }
 
   public static function get_level_by_code($code)
