7b7aaf0960af04e24681e2aceafbd6a51d658db0
maneptha <maneptha@gmail.com>
2016-02-16 22:40:18 +0100

    student courses model - can create course

--- admin_academics/models/Courses.php
+++ admin_academics/models/Courses.php
@@ -12,7 +12,7 @@ class Courses1
   /**
    * Get the courses that can be taken in a particular department in a particular semester, and optionally in a level
    *
-   * @param array $data - an array that holds the department code and semester number (1 or 2), of the form:
+   * @param array $params - an array that holds the department code and semester number (1 or 2), of the form:
    * [
    *  'department' => string, 'semester' => number|string, 'level' => string
    * ]
@@ -20,21 +20,21 @@ class Courses1
    *
    * @return array|null
    */
-  public static function getCoursesForSemesterDeptLevel(array $data)
+  public static function getCoursesForSemesterDeptLevel(array $params)
   {
     $query = "SELECT * FROM course_table
               WHERE department = :department
               AND semester = :semester";
 
-    if (isset($data['level'])) $query .= ' AND class = :level';
+    if (isset($params['level'])) $query .= ' AND class = :level';
 
     $logMessage = SqlLogger::makeLogMessage(
-      'get courses for a particular department in a particular semester and optionally, level', $query, $data
+      'get courses for a particular department in a particular semester and optionally, level', $query, $params
     );
 
     $stmt = get_db()->prepare($query);
 
-    if ($stmt->execute($data)) {
+    if ($stmt->execute($params)) {
       SqlLogger::logStatementSuccess(self::logger(), $logMessage);
 
       $result = $stmt->fetchAll();
@@ -46,6 +46,61 @@ class Courses1
     }
 
     SqlLogger::logNoData(self::logger(), $logMessage);
+    return null;
+  }
+
+
+  /**
+   * Get all courses
+   *
+   * @return array|null
+   */
+  public static function getAllCourses()
+  {
+    $query = "SELECT * FROM course_table";
+
+    $logger = new SqlLogger(self::logger(), 'get all courses', $query);
+
+    $stmt = get_db()->query($query);
+
+    if ($stmt) {
+      $logger->statementSuccess();
+
+      $result = $stmt->fetchAll();
+
+      if (count($result)) {
+        $logger->dataRetrieved($result);
+        return $result;
+      }
+    }
+
+    $logger->noData();
+    return null;
+  }
+
+  /**
+   * @param array $params
+   *
+   * * @return array|null
+   */
+  public static function createCourse(array $params)
+  {
+    $query = "INSERT INTO course_table (title, code, department, class, semester, active, unit)
+              VALUE (:title, :code, :department, :class, :semester, :active, :unit)";
+
+    $logger = new SqlLogger(self::logger(), 'create course ', $query, $params);
+
+    $db = get_db();
+    $stmt = $db->prepare($query);
+
+    if ($stmt->execute($params)) {
+      $logger->statementSuccess();
+      $params['id'] = $db->lastInsertId();
+      $logger->dataRetrieved($params);
+      return $params;
+    }
+
+    $logger->noData();
     return null;
   }
 
